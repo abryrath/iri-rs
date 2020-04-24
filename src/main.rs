956a2b39259cf8c3433901638a714a28539e9be4
Abry Rath <abry.rath@union.co>
2020-04-24 09:57:23 -0400

    Example on gpio 23/24

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate gpio;
+use gpio::{GpioOut,GpioIn};
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let mut gpio23 = gpio::sysfs::SysFsGpioInput::open(23).unwrap();
+    let mut gpio24 = gpio::sysfs::SysFsGpioOutput::open(24).unwrap();
+    let mut value = true;
+    thread::spawn(move || loop {
+        gpio24.set_value(value).expect("Could not set GPIO24");
+        thread::sleep(time::Duration::from_millis(1000));
+        value = !value;
+    });
+    loop {
+        println!("GPIO23: {:?}", gpio23.read_value().unwrap());
+        thread::sleep(time::Duration::from_millis(100));
+    }
 }
